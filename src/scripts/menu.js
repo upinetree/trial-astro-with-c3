65a39bd1531bbd0a08d652dc97f7e343e8d851be
upinetree <upinetree@gmail.com>
2023-08-30 14:02:17 +0000

    use layout!

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,4 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("expanded");
+  document.querySelector(".clock").classList.toggle("hidden");
 });
