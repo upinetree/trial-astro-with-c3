255f922f6b6a16eaf3da65df84af8b8c57aab8c0
upinetree <upinetree@gmail.com>
2023-09-03 14:24:03 +0000

    enable view transitions and adjust scripts

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,7 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".nav-links").classList.toggle("expanded");
-  document.querySelector(".clock").classList.toggle("hidden");
-  document.querySelector(".theme-icon").classList.toggle("hidden");
+document.addEventListener("astro:page-load", () => {
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("expanded");
+    document.querySelector(".clock").classList.toggle("hidden");
+    document.querySelector(".theme-icon").classList.toggle("hidden");
+  });
 });
