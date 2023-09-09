36e022eb549cc01b1e57fdc3e57ef8db852adbf8
upinetree <upinetree@gmail.com>
2023-09-09 13:52:12 +0000

    Introduce tailwindcss

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,8 +9,15 @@ export default function Greeting({ messages }) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+      <h3 class="text-2xl mb-6">{greeting}! Thank you for visiting!</h3>
+      <p class="my-4">
+        <button
+          class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          onClick={() => setGreeting(randomMessage())}
+        >
+          New Greeting
+        </button>
+      </p>
     </div>
   );
 }
