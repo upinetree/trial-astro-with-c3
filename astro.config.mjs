3ba0a467ddbfd3492943332057d92dfe982ec0dd
upinetree <upinetree@gmail.com>
2023-09-09 03:15:19 +0000

    :run: npx astro add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
